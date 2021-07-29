65ce2e03d23f601c106180b8eda5a25a87f20739
AnthonySzymczak <doyalajs1@live.com>
2021-07-29 16:07:10 -0400

    working on index.js, inquirer and function setup

--- index.js
+++ index.js
@@ -6,8 +6,75 @@ const consoleTable = require('console.table');
 
 
 
+init();
+function init() {
+    inquirer.prompt({
+            type: "list",
+            choices: [
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Update Employee Role",
+                "Delete Employee",
+                "Delete Role",
+                "Delete Department",
+                "Quit"
+            ],
+            message: "What would you like to do?",
+            name: "selection"
+        })
+        .then((answer) => {
+            
+            switch (answer.selection) {
 
+                case "View Employees":
+                    viewAllEmployees();
+                    break;
 
+                case "View Roles":
+                    viewAllRoles();
+                    break;
+
+                case "View Departments":
+                    viewAllDepartments();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Add Role":
+                    addRole();
+                    break;
+
+                case "Add Department":
+                    addDepartment();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployee();
+                    break;
+
+                case "Delete Employee":
+                    deleteEmployee();
+                    break;
+
+                case "Delete Role":
+                    deleteRole();
+                    break;
+
+                case "Delete Department":
+                    deleteDepartment();
+                    break;
+
+                case "Quit":
+                    quit();
+            }
+        });
+}
 
 
 
